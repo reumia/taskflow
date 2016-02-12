74481809937c0bf2bd19ec75ff655f5be648bc42
hyunsukjo <hyunsuk.jo@nhn.com>
2016-02-12 15:14:29 +0900

    package.json, gulpfile.js 설정 수정

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,12 @@ gulp.task('sass', function() {
     return gulp.src('./src/scss/app.scss')
     	.pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write('./src/css/maps'))
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./src/css'));
 });
 
-gulp.task('sass:watch', function() {
+gulp.task('watch', function() {
     gulp.watch('./src/scss/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass:watch']);
+gulp.task('default', ['watch']);
