26c0b1c6d76b3fe4a002a5d2e9ffa4105f8ecc4e
hyunsukjo <hyunsuk.jo@nhn.com>
2016-02-15 18:38:06 +0900

    Task : 이벤트 추가, html 정리

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 // Task
 var Task = React.createClass({
-    getDateByTimeStamp : function(string){
+    getDateByTimeStamp: function (string) {
         var timestamp = new Date(string);
         if (timestamp.getTime() > 0) {
             return timestamp.toLocaleDateString();
@@ -8,13 +8,16 @@ var Task = React.createClass({
             return string;
         }
     },
+    handleClick: function () {
+        console.log('Task Clicked!');
+    },
     render: function () {
         var data = this.props.data;
         var inlineStyleColor = {color: data.categoryColor};
         var inlineStyleBackgroundColor = {backgroundColor: data.categoryColor};
         var deployDate = this.getDateByTimeStamp(data.deploy);
         return (
-            <article className="task">
+            <article className="task" onClick={this.handleClick}>
                 <h3 className="task__category" style={inlineStyleColor}>
                     <i className="sticker" style={inlineStyleBackgroundColor}></i>
                     <em>{data.category}</em>
@@ -44,17 +47,20 @@ var Task = React.createClass({
 
 // TaskWrap
 var TaskWrap = React.createClass({
+    getInitialState: function() {
+        return {data: []}
+    },
     render: function () {
         var data = this.props.data;
+        var event = this.handleClickTask;
         var taskNodes = Object.keys(data).map(function(key) {
             var item = data[key];
             return (
-                <Task data={item} statement={key} key={item.date}/>
+                <Task data={item} statement={key} key={item.date} />
             );
         });
         return (
             <div className="task-wrap list__item">
-                <h1>{this.props.statement}</h1>
                 {taskNodes}
             </div>
         );
@@ -93,24 +99,13 @@ var TaskFlow = React.createClass({
 var myStorage = {
     "todo" : [
         {"date":1455523409565,"category":"GRAFOLIO","categoryColor":"#2ecc71","title":"[기타] 다이어리 팝업에서 제목 표시줄에 있는 아이콘이 기본 브라우저 아이콘으로 노출됨","deploy":1458226800000,"origin":"GRAFOLIO-3000","originLink":"#","detail":["aaa", "bbb"]},
-        {"date":1455523409566,"category":"GRAFOLIO","categoryColor":"#2ecc71","title":"테스트 ㅎㅎ","deploy":"미정","origin":"E-mail","originLink":"#","detail":["aaa", "bbb"]}
+        {"date":1455523409566,"category":"NONE","categoryColor":"","title":"테스트 ㅎㅎ","deploy":"미정","origin":"E-mail","originLink":"#","detail":["aaa", "bbb", "ccccddd"]}
     ],
     "progress" : [
-        {"date":1455523409567,"category":"LINEPLAY-DIARY","categoryColor":"red","title":"하트를 준 아바타 : 아바타 위치 수정","deploy":"미정","origin":"E-mail","originLink":"#","detail":["aaa", "bbb"]},
-        {"date":1455523409568,"category":"GRAFOLIO","categoryColor":"#2ecc71","title":"[기타] 다이어리 팝업에서 제목 표시줄에 있는 아이콘이 기본 브라우저 아이콘으로 노출됨","deploy":"미정","origin":"E-mail","originLink":"#","detail":["aaa", "bbb"]},
-        {"date":1455523409569,"category":"GRAFOLIO","categoryColor":"#2ecc71","title":"[기타] 다이어리 팝업에서 제목 표시줄에 있는 아이콘이 기본 브라우저 아이콘으로 노출됨","deploy":"미정","origin":"E-mail","originLink":"#","detail":["aaa", "bbb"]}
+        {"date":1455523409567,"category":"LINEPLAY-DIARY","categoryColor":"red","title":"하트를 준 아바타 : 아바타 위치 수정","deploy":"미정","origin":"E-mail","originLink":"#","detail":[]}
     ],
-    "done" : [
-        {"date":1455523409571,"category":"GRAFOLIO","categoryColor":"#2ecc71","title":"[기타] 다이어리 팝업에서 제목 표시줄에 있는 아이콘이 기본 브라우저 아이콘으로 노출됨","deploy":"미정","origin":"E-mail","originLink":"#","detail":["aaa", "bbb"]},
-        {"date":1455523409572,"category":"GRAFOLIO","categoryColor":"#2ecc71","title":"[기타] 다이어리 팝업에서 제목 표시줄에 있는 아이콘이 기본 브라우저 아이콘으로 노출됨","deploy":"미정","origin":"E-mail","originLink":"#","detail":["aaa", "bbb"]},
-        {"date":1455523409573,"category":"GRAFOLIO","categoryColor":"#2ecc71","title":"[기타] 다이어리 팝업에서 제목 표시줄에 있는 아이콘이 기본 브라우저 아이콘으로 노출됨","deploy":"미정","origin":"E-mail","originLink":"#","detail":["aaa", "bbb"]}
-    ],
-    "next" : [
-        {"date":1455523409574,"category":"GRAFOLIO","categoryColor":"#2ecc71","title":"[기타] 다이어리 팝업에서 제목 표시줄에 있는 아이콘이 기본 브라우저 아이콘으로 노출됨","deploy":"미정","origin":"E-mail","originLink":"#","detail":["aaa", "bbb"]},
-        {"date":1455523409575,"category":"GRAFOLIO","categoryColor":"#2ecc71","title":"[기타] 다이어리 팝업에서 제목 표시줄에 있는 아이콘이 기본 브라우저 아이콘으로 노출됨","deploy":"미정","origin":"E-mail","originLink":"#","detail":["aaa", "bbb"]},
-        {"date":1455523409576,"category":"GRAFOLIO","categoryColor":"#2ecc71","title":"[기타] 다이어리 팝업에서 제목 표시줄에 있는 아이콘이 기본 브라우저 아이콘으로 노출됨","deploy":"미정","origin":"E-mail","originLink":"#","detail":["aaa", "bbb"]},
-        {"date":1455523409577,"category":"GRAFOLIO","categoryColor":"#2ecc71","title":"[기타] 다이어리 팝업에서 제목 표시줄에 있는 아이콘이 기본 브라우저 아이콘으로 노출됨","deploy":"미정","origin":"E-mail","originLink":"#","detail":["aaa", "bbb"]}
-    ]
+    "done" : [],
+    "next" : []
 }
 
 // Render
