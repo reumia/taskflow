0737efcd090960d1d5a32ca5584624a42d92b493
zzoman <reumia@gmail.com>
2016-03-19 05:25:12 +0900

    CategoryConfig : 목록 생성

--- src/js/CategoryConfig.js
+++ src/js/CategoryConfig.js
@@ -4,10 +4,19 @@ var ClassNames = require('classnames');
 
 var CategoryConfig = React.createClass({
     render: function () {
+        var categories = this.props.categories;
         var classes = ClassNames({
             'layer': true,
-            'layer_config_category': true,
+            'layer--config-category': true,
             'active': this.props.isActivated
+        });
+        var categoryNodes = Object.keys(categories).map(function(key){
+            var category = categories[key];
+            var inlineStyleColor = {color: category.color};
+            var inlineStyleBackgroundColor = {backgroundColor: category.color};
+            return (
+                <a className="item" href="#" style={inlineStyleColor}><i className="sticker" style={inlineStyleBackgroundColor}></i>{category.name}</a>
+            );
         });
         return (
             <div className={classes}>
@@ -18,6 +27,9 @@ var CategoryConfig = React.createClass({
                         <input type="text" className="input" placeholder="카테고리 색상"/>
                         <input type="text" className="input" placeholder="카테고리 이름"/>
                         <a href="#" className="button button--block"><i className="fa fa-plus"></i> 추가</a>
+                        <div className="item-group">
+                            {categoryNodes}
+                        </div>
                     </div>
                 </div>
             </div>
